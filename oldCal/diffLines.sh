4cfdc3b6428212f3df8448b26ae8a62a04592554
s-bessey <sarah_bessey@brown.edu>
2019-08-20 16:04:40 -0400

    remove extraneous files that should've been ignored

--- oldCal/diffLines.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-$1 = old
-$2 = new
-diff --unchanged-line-format="" --new-line-format="%dn: %L" --old-line-format="%dn: %L" $old $new
-
--- oldCal/memtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in 1 3 5 7 13
-do
-    ./subTitan.sh calTrt.py -N 55000 -j memory_$i -m $i
-done
