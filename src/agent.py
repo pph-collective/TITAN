e8db412a7969d7634e1eb6fed1619183985ccdfa
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-09-24 14:43:35 -0400

    fix: agent uses random, added import

--- src/agent.py
+++ src/agent.py
@@ -40,6 +40,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *****************************************************************************
 """
 
+import random
 
 class Agent(object):
     "Class for agent objects."
