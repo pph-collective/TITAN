c249ca1191f86a6327598d7542326532bb2d258b
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-10-03 11:07:01 -0400

    fix: fix import graphviz_layout

--- src/network_graph_tools.py
+++ src/network_graph_tools.py
@@ -7,6 +7,7 @@ import collections
 
 import numpy as np
 import networkx as nx
+from ns.nx_agraph import graphviz_layout
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from operator import itemgetter
