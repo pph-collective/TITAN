84cbd72356e550e7a4ecf6c24c03fe6d7b85dba4
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-11-06 18:22:18 -0500

    style: make integration test more readable, fixes black error

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -7,12 +7,6 @@ def test_model_runs():
     g = globals().copy()
     g["__name__"] = "__main__"
 
-    exec(
-        open(
-            os.path.join(
-                os.path.dirname(os.path.abspath(__file__)), "..", "run_titan.py"
-            )
-        ).read(),
-        g,
-    )
+    f = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "run_titan.py")
+    exec(open(f).read(), g)
     assert True
