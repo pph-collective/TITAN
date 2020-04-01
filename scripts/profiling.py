5edecbfb41380994eab8963ec46cd12b58aae216
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2020-04-01 16:52:42 -0400

    feat: sweep parameters via command line

--- scripts/profiling.py
+++ scripts/profiling.py
@@ -3,7 +3,7 @@ import run_titan
 
 pr = cProfile.Profile()
 pr.enable()
-run_titan.main("custom", "tests/params/basic_seeded.yml", 1, "results", True)
+run_titan.main("custom", "tests/params/basic_seeded.yml", 1, "results", True, [], False)
 
 pr.disable()
 s = io.StringIO()
