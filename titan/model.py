a6c647ca2a8e05f54778674ec00c21a5ecbaa71a
sbessey <sarah_bessey@brown.edu>
2020-05-25 17:51:50 -0400

    feat: allow choice of bins or dist for act frequency. abstract get_number_of_acts, not yet reflected in params

--- titan/model.py
+++ titan/model.py
@@ -675,7 +675,7 @@ class HIVModel:
 
         # unprotected sex probabilities for primary partnerships
         mean_sex_acts = (
-            agent.get_number_of_sex_acts(self.run_random, self.params)
+            agent.get_number_of_acts(self.run_random, self.params, "sex")
             * self.calibration.sex.act
         )
         total_sex_acts = utils.poisson(mean_sex_acts, self.np_random)
