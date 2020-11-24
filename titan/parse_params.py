b0f8761008a9bfc9443478f1410299beb11b1276
sbessey <sarah_bessey@brown.edu>
2020-11-24 16:10:57 -0500

    style: run black

--- titan/parse_params.py
+++ titan/parse_params.py
@@ -279,7 +279,9 @@ def check_params(params):
         assort_value = 0
         for ptnr_value in assort.partner_values.values():
             assort_value += ptnr_value
-        assert math.isclose(assort_value, 1, abs_tol=0.001), f"assort values must add to 1, not {assort_value} in {param}"
+        assert math.isclose(
+            assort_value, 1, abs_tol=0.001
+        ), f"assort values must add to 1, not {assort_value} in {param}"
 
 
 def warn_unused_params(parsed, params, base, key_path):
