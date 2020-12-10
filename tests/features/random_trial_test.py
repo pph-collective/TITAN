2f5a2883899a3ff86a1acc0230ee435d8145f844
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2020-12-10 17:28:22 -0500

    test: random trial all choices

--- tests/features/random_trial_test.py
+++ tests/features/random_trial_test.py
@@ -24,6 +24,57 @@ def test_initialize_random_trial_prep_all(make_model, params):
 
 
 @pytest.mark.unit
+def test_initialize_random_trial_prep_eigenvector(make_model, params):
+    params.features.prep = True
+    params.vaccine.on_init = False
+    params.prep.target = 0
+    params.hiv.start_time = 5
+    params.random_trial.choice = "eigenvector"
+    model = make_model(params)
+    model.run_random = FakeRandom(-0.1)
+    model.time = model.params.random_trial.start_time
+    RandomTrial.update_pop(model)
+    num_treated = 0
+    num_suitable = 0
+    for agent in model.pop.all_agents:
+        assert agent.random_trial.active
+        if agent.random_trial.treated:
+            num_treated += 1
+        if agent.random_trial.suitable:
+            num_suitable += 1
+
+    num_components = len(model.pop.connected_components())
+
+    assert num_treated == num_components
+    assert num_suitable == num_components
+
+
+@pytest.mark.unit
+def test_initialize_random_trial_prep_random(make_model, params):
+    params.features.prep = True
+    params.vaccine.on_init = False
+    params.prep.target = 0
+    params.hiv.start_time = 5
+    params.random_trial.choice = "random"
+    model = make_model(params)
+    model.run_random = FakeRandom(-0.1)
+    model.time = model.params.random_trial.start_time
+    RandomTrial.update_pop(model)
+    num_treated = 0
+    num_suitable = 0
+    for agent in model.pop.all_agents:
+        assert agent.random_trial.active
+        if agent.random_trial.treated:
+            num_treated += 1
+        if agent.random_trial.suitable:
+            num_suitable += 1
+
+    num_components = len(model.pop.connected_components())
+
+    assert num_treated == num_components
+    assert num_suitable == num_components
+
+@pytest.mark.unit
 def test_initialize_random_trial_pca_bridge(make_model, params):
     # knowledge bridge trial
     params.random_trial.treatment = "knowledge"
