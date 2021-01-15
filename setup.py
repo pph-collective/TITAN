09ad1ad9c57451f2215662204a3f94f4d847691c
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2021-01-15 12:12:24 -0500

    build: add setup.py so titan can be installed with pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="titan", # Replace with your own username
+    version="2.0.0",
+    # author="Example Author",
+    # author_email="author@example.com",
+    description="TITAN Agent Based Model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marshall-lab/TITAN",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
