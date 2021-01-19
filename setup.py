754b0f976c29d6b204beb3152c2e5b2e503b346d
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2021-01-19 09:13:00 -0500

    fix: add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="titan", # Replace with your own username
     version="2.0.0",
@@ -13,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/marshall-lab/TITAN",
     packages=setuptools.find_packages(),
+    install_requires=required,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
