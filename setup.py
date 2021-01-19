cd204ca04f8e70713009cb25b0d94b8bcc12b806
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2021-01-19 09:53:08 -0500

    build: debugging print

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from pathlib import Path
 def get_active_branch_name():
     head_dir = Path(".") / ".git" / "HEAD"
     with head_dir.open("r") as f: content = f.read().splitlines()
-
     for line in content:
+        print(line)
         if line[0:4] == "ref:":
             return line.partition("refs/heads/")[2]
 
