b7261ce941e6bd5812ed4e86c6c220508dc42eb3
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2021-01-19 10:30:49 -0500

    build: lift/edit get-git-vesion

--- setup.py
+++ setup.py
@@ -1,4 +1,41 @@
 import setuptools
+import subprocess
+
+# ========= FROM https://github.com/vivin/better-setuptools-git-version/blob/master/better_setuptools_git_version.py with edits =========
+def get_latest_tag():
+    subprocess.getoutput("git describe --tags `git rev-list --tags --max-count=1`")
+
+def get_tag_commit_sha(tag):
+    """Return the commit that the tag is pointing to."""
+    return subprocess.getoutput("git rev-list -n 1 {tag}".format(tag=tag))
+
+def get_head_sha():
+    """Return the sha key of HEAD."""
+    return subprocess.getoutput('git rev-parse HEAD')
+
+def is_head_at_tag(tag):
+    """Return True or False depending on whether the given tag is pointing to HEAD"""
+    return get_head_sha() == get_tag_commit_sha(tag)
+
+def get_version():
+    """
+    Return the full git version using the given template. If there are no annotated tags, the version specified by
+    starting_version will be used. If HEAD is at the tag, the version will be the tag itself. If there are commits ahead
+    of the tag, the first 8 characters of the sha of the HEAD commit will be included.
+    In all of the above cases, if the working tree is also dirty or contains untracked files, a "+dirty" suffix will be
+    appended to the version.
+    Returns:
+        the formatted version based on tags in the git repository.
+    """
+    template="{tag}.dev{sha}"
+    tag = get_latest_tag()
+    if is_head_at_tag(tag):
+        version = tag
+    else:
+        sha = get_head_sha()[:8]
+        version = template.format(tag=tag, sha=sha)
+
+    return version
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -8,11 +45,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="titan", # Replace with your own username,
-    version_config={
-        "version_format": "{tag}.dev{sha}",
-        "starting_version": "0.1.0"
-    },
-    setup_requires=['better-setuptools-git-version'],
+    version=get_version(),
     # author="Example Author",
     # author_email="author@example.com",
     description="TITAN Agent Based Model",
