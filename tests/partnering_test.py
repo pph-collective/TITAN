1660576f5ff21a14b6220b50920e71de0332a428
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2021-03-19 10:56:13 -0400

    feat: assort rules by bond_type

--- tests/partnering_test.py
+++ tests/partnering_test.py
@@ -171,6 +171,7 @@ def test_get_assort_partner_race(make_population, make_agent, params):
         {
             "attribute": "race",
             "partner_attribute": "__agent__",
+            "bond_types": [],
             "agent_value": "white",
             "partner_values": {"white": 0.9, "__other__": 0.1},
         }
@@ -194,6 +195,7 @@ def test_get_assort_partner_race(make_population, make_agent, params):
         {
             "attribute": "race",
             "partner_attribute": "__agent__",
+            "bond_types": [],
             "agent_value": "white",
             "partner_values": {"white": 0.9, "__other__": 10},
         }
@@ -245,6 +247,7 @@ def test_get_assort_partner_high_risk(make_population, make_agent, params):
         {
             "attribute": "high_risk",
             "partner_attribute": "__agent__",
+            "bond_types": [],
             "agent_value": True,
             "partner_values": {"True": 0.9, "__other__": 0.1},
         }
@@ -268,6 +271,7 @@ def test_get_assort_partner_high_risk(make_population, make_agent, params):
         {
             "attribute": "high_risk",
             "partner_attribute": "__agent__",
+            "bond_types": [],
             "agent_value": True,
             "partner_values": {"True": 0.9, "__other__": 10},
         }
@@ -323,6 +327,7 @@ def test_get_assort_partner_drug_type(make_population, make_agent, params):
         {
             "attribute": "drug_type",
             "partner_attribute": "__agent__",
+            "bond_types": [],
             "agent_value": "Inj",
             "partner_values": {"Inj": 0.8, "NonInj": 0.1, "__other__": 0.1},
         }
@@ -346,6 +351,7 @@ def test_get_assort_partner_drug_type(make_population, make_agent, params):
         {
             "attribute": "drug_type",
             "partner_attribute": "__agent__",
+            "bond_types": [],
             "agent_value": "Inj",
             "partner_values": {"Inj": 0.8, "NonInj": 0.1, "__other__": 10},
         }
@@ -388,6 +394,7 @@ def test_get_assort_same_race(make_population, make_agent, params):
         {
             "attribute": "race",
             "partner_attribute": "__agent__",
+            "bond_types": [],
             "agent_value": "__any__",
             "partner_values": {"__same__": 0.9, "__other__": 0.1},
         }
@@ -462,6 +469,7 @@ def test_get_assort_hiv(make_population, make_agent, params):
         {
             "attribute": "hiv.active",
             "partner_attribute": "__agent__",
+            "bond_types": [],
             "agent_value": True,
             "partner_values": {"True": 0.9, "False": 0.1},
         }
@@ -485,6 +493,7 @@ def test_get_assort_hiv(make_population, make_agent, params):
         {
             "attribute": "hiv.active",
             "partner_attribute": "__agent__",
+            "bond_types": [],
             "agent_value": True,
             "partner_values": {"True": 0.9, "False": 10},
         }
@@ -530,6 +539,7 @@ def test_get_assort_hiv_prep(make_population, make_agent, params):
         {
             "attribute": "hiv.active",
             "partner_attribute": "prep.active",
+            "bond_types": [],
             "agent_value": True,
             "partner_values": {"True": 0.9, "False": 0.1},
         }
@@ -553,6 +563,7 @@ def test_get_assort_hiv_prep(make_population, make_agent, params):
         {
             "attribute": "hiv.active",
             "partner_attribute": "prep.active",
+            "bond_types": [],
             "agent_value": True,
             "partner_values": {"True": 0.9, "False": 10},
         }
@@ -570,6 +581,72 @@ def test_get_assort_hiv_prep(make_population, make_agent, params):
     )
 
     assert partner == p2
+
+
+@pytest.mark.unit
+def test_get_assort_bond_type(make_population, make_agent, params):
+    pop = make_population()
+    a = make_agent(SO="MSM", race="white")
+    p = make_agent(SO="MSM", race="white")
+    a.hiv.active = True
+    p.hiv.active = True
+    for bond in params.classes.bond_types:
+        a.target_partners[bond] = 1
+        p.target_partners[bond] = 1
+        a.partners[bond] = set()
+        p.partners[bond] = set()
+    pop.add_agent(a)
+    pop.add_agent(p)
+
+    params.features.assort_mix = True
+
+    # assort with white
+    test_rule = ObjMap(
+        {
+            "attribute": "hiv.active",
+            "partner_attribute": "__agent__",
+            "bond_types": ["Social"],
+            "agent_value": True,
+            "partner_values": {"True": 0.1, "False": 0.9},
+        }
+    )
+    params.assort_mix["test_rule"] = test_rule
+
+    partner = select_partner(
+        a,
+        pop.all_agents.members,
+        pop.sex_partners,
+        pop.pwid_agents,
+        params,
+        FakeRandom(0.5),
+        "Social",
+    )
+
+    assert partner is None
+
+    # get __other__
+    test_rule = ObjMap(
+        {
+            "attribute": "hiv.active",
+            "partner_attribute": "__agent__",
+            "bond_types": ["Sex"],
+            "agent_value": True,
+            "partner_values": {"True": 0.9, "False": 0.1},
+        }
+    )
+    params.assort_mix["test_rule"] = test_rule
+
+    partner = select_partner(
+        a,
+        pop.all_agents.members,
+        pop.sex_partners,
+        pop.pwid_agents,
+        params,
+        FakeRandom(0.5),
+        "Sex",
+    )
+
+    assert partner == p
 
 
 @pytest.mark.unit
