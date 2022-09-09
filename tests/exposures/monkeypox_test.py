876bdb58a2cbadd79eaedbb61e1a6955455b3cb9
Sam Bessey <sam_bessey@brown.edu>
2022-09-09 10:09:40 -0400

    test(monkeypox): add tests for monkeypox init_agent and transmission prob for insertive/versatile relationship

--- tests/exposures/monkeypox_test.py
+++ tests/exposures/monkeypox_test.py
@@ -42,6 +42,29 @@ def test_monkeypox_expose(make_model, make_agent):
 
 
 @pytest.mark.unit
+def test_monkeypox_init(make_population, make_agent):
+    pop = make_population()
+    pop.pop_random = FakeRandom(0.0)
+    a = make_agent()
+
+    time = pop.params.monkeypox.start_time - 1
+
+    a.monkeypox.init_agent(pop, time)
+    assert a.monkeypox.active is False
+
+    time = pop.params.monkeypox.start_time
+
+    a.monkeypox.init_agent(pop, time)
+
+    assert a.monkeypox.active
+    assert a.monkeypox.time == time - pop.params.monkeypox.max_init_time
+    assert a.monkeypox.dx
+    assert a.monkeypox.dx_time == a.monkeypox.time
+    assert a in MonkeyPox.agents
+    assert MonkeyPox.dx_counts[a.race][a.sex_type] == 1
+
+
+@pytest.mark.unit
 def test_monkeypox_convert(make_model, make_agent):
     model = make_model()
     a = make_agent()
@@ -82,6 +105,7 @@ def test_get_transmission_probability(make_model, make_agent):
     p = make_agent(race="white", SO="MSM")
     p.sex_role = "versatile"
 
+    # test versatile-versatile relationship
     p_sex = model.params.partnership.sex.acquisition.MSM.versatile
     scale = model.params.calibration.acquisition
 
@@ -90,6 +114,16 @@ def test_get_transmission_probability(make_model, make_agent):
     a.monkeypox.time = model.time
 
     assert a.monkeypox.get_transmission_probability(model, "sex", p, 1) == p_sex * scale
+    # Not transmissable with injection
+    assert a.monkeypox.get_transmission_probability(model, "inj", p, 1) == 0.0 
+
+    # test one vers, one receptive
+    a.sex_role = "receptive"
+    p_sex_ins = model.params.partnership.sex.acquisition.MSM.insertive
+    p_sex_rec = model.params.partnership.sex.acquisition.MSM.receptive
+
+    assert a.hiv.get_transmission_probability(model, "sex", p, 1) == p_sex_ins * scale
+    assert p.hiv.get_transmission_probability(model, "sex", a, 1) == p_sex_rec * scale
 
 
 @pytest.mark.unit
